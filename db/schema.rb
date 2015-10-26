cdd76882e4da3ba17c42eb9f9c0968000c919a48
astrung <tuananh93nguyen@gmail.com>
2015-10-26 07:18:43 +0000

    modify User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019082631) do
+ActiveRecord::Schema.define(version: 20151026070155) do
 
   create_table "goals", force: :cascade do |t|
     t.integer  "user_id"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20151019082631) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "gender"
+    t.integer  "age"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
